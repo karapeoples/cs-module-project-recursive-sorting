2875b480edbb7a27ac0675510e4e59d77f8a8655
karapeoples <kararpeoples@yahoo.com>
2020-10-05 21:54:44 -0500

    Code In Now Need Notes MVP MET

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,28 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-
+    #%BASE CASE%#
+    if end >= start:
+        #%Make a MidPoint%#
+        middle = (start + end)//2
+        #%If the Middle is the target return middle%#
+        if arr[middle] == target:
+            return middle
+        #%Make a Smaller then Middle Case and Larger than Middle Case%#
+        elif arr[middle] < target:
+            return binary_search(arr, target, middle + 1, end)
+        else:
+            return binary_search(arr, target, start, middle - 1)
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,25 +4,47 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    arrA_pointer = arrB_pointer = 0
+    arrA_length, arrB_length = len(arrA), len(arrB)
 
+    for _ in range(elements):
+        if arrA_pointer < arrA_length and arrB_pointer < arrB_length:
+            if arrA[arrA_pointer] <= arrB[arrB_pointer]:
+                merged_arr[arrA_pointer + arrB_pointer] = arrA[arrA_pointer]
+                arrA_pointer += 1
+            else:
+                merged_arr[arrA_pointer + arrB_pointer] = arrB[arrB_pointer]
+                arrB_pointer += 1
+
+        elif arrA_pointer == arrA_length:
+            merged_arr[arrA_pointer + arrB_pointer] = arrB[arrB_pointer]
+            arrB_pointer += 1
+
+        elif arrB_pointer == arrB_length:
+            merged_arr[arrA_pointer + arrB_pointer] = arrA[arrA_pointer]
+            arrA_pointer += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) <= 1: return arr
+    midpoint = int(len(arr) // 2)
+    arrA = merge_sort(arr[:midpoint])
+    arrB = merge_sort(arr[midpoint:])
 
+    return merge(arrA, arrB)
 
-    return arr
-
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
 
